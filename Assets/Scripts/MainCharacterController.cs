0f14c3708934905a7fd444af0077913f2db93c44
Jedd Goble <jedd@Jedds-MacBook-Pro.local>
2017-07-14 16:37:23 -0700

    First commit

---
+++ Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+
+	public float speed = 14f;
+	private Vector2 input;
+	private SpriteRenderer sr;
+	private Rigidbody2D rb;
+	private Animator animator;
+
+	private float width;
+	private float height;
+
+	void Awake()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		//animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+
+		width = GetComponent<BoxCollider2D>().bounds.extents.x + 0.1f;
+		height = GetComponent<BoxCollider2D>().bounds.extents.y + 0.2f;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		input.x = Input.GetAxis("Horizontal");
+		input.y = Input.GetAxis("Vertical");
+
+
+
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector2(input.x* speed, input.y* speed);
+	}
+}
