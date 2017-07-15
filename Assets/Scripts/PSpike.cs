e36552cc6a996d36153a237db1f2e869096be35e
Armando <kouryu@gmail.com>
2017-07-15 12:27:18 -0500

    Spikes despawn

--- Assets/Scripts/PSpike.cs
+++ Assets/Scripts/PSpike.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PSpike : MonoBehaviour {
 
+    private float timer = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,15 @@ public class PSpike : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if(timer > 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
