9ec4cd54c333bfbd790b75100a9c51232f4cbf13
Armando <kouryu@gmail.com>
2017-07-15 14:45:25 -0500

    complete saw blade

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
---
+++ Assets/Scripts/SawHolder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawHolder : MonoBehaviour {
+
+    private SliderJoint2D sj;
+
+    public float slideSpeed = 2;
+
+    // Use this for initialization
+    void Start () {
+        sj = GetComponent<SliderJoint2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (sj.limitState == JointLimitState2D.UpperLimit)
+        {
+            JointMotor2D motor = new JointMotor2D();
+            motor.motorSpeed = -slideSpeed;
+            motor.maxMotorTorque = 10000;
+            sj.motor = motor;
+        }
+        else if (sj.limitState == JointLimitState2D.LowerLimit)
+        {
+            JointMotor2D motor = new JointMotor2D();
+            motor.motorSpeed = slideSpeed;
+            motor.maxMotorTorque = 10000;
+            sj.motor = motor;
+        }
+    }
+}
