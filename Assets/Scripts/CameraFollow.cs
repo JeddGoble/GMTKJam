5dec2e27c72d6d9fcc5e24a45c7011965bc89d32
Jedd Goble <jedd@Jedds-MacBook-Pro.local>
2017-07-14 17:09:26 -0700

    Added smooth camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public Transform camTarget;
+	public float trackingSpeed = 3f;
+
+	void FixedUpdate()
+	{
+		if (camTarget != null)
+		{
+			var targetPos = new Vector2(camTarget.position.x / 4f, camTarget.position.y / 4f);
+
+			var newPos = Vector2.Lerp(transform.position,
+			                          targetPos,
+						  Time.deltaTime * trackingSpeed);
+			var camPosition = new Vector3(newPos.x, newPos.y, -10f);
+			transform.position = camPosition;
+		}
+	}
+}
