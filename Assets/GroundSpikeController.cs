2712ad52b9775e87b7e22d908ecf5432e53ffb48
Jedd Goble <jedd@Jedds-MacBook-Pro.local>
2017-07-15 06:32:56 -0700

    Added ground spikes

---
+++ Assets/GroundSpikeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpikeController : MonoBehaviour
+{
+
+	public Sprite spikesUpSprite;
+	public Sprite spikesDownSprite;
+	public float delay;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			spriteRenderer.sprite = spikesUpSprite;
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			spriteRenderer.sprite = spikesDownSprite;
+		}
+	}
+}
